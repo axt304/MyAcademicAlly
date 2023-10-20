d9a6e0ffa41f449cf6b6e792c65f8f9b3c9bd133
Michael <--global>
2023-10-19 22:37:18 -0400

    Added login logic for prototype.

--- server/main.py
+++ server/main.py
@@ -1,24 +1,54 @@
 import uvicorn
 from typing import Annotated
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
 
-from server.model import User
+from server.model import User, fake_users_db
+
+
 
 app = FastAPI()
 
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+#Example user stored in database
+class UserInDB(User):
+    hashed_password: str
+
+#Example helper for prototype
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+    
 #Example token decoder
 def fake_decode_token(token):
-    return User(
-        username = token + "fakecode", email="johndoe@example.com", fullName="John Doe"
-    )
+    # This doesn't provide any security at all
+    user = get_user(fake_users_db, token)
+    return user
 
 #User credentials
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
+
+#User credetials of active user
+async def get_current_active_user(
+        current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 #Test
 @app.get("/", tags=["test"])
@@ -34,3 +64,16 @@ async def read_tasks(token: Annotated[str, Depends(oauth2_scheme)]):
 @app.get("/users/me")
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
+
+#Example login function
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        
+    return {"access_token": user.username, "token_type": "bearer"}
--- server/model.py
+++ server/model.py
@@ -6,3 +6,21 @@ class User(BaseModel):
     email: str | None = None
     full_name: str  | None = None
     disabled: bool  | None = None 
+
+#Fake user data for prototype
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "janedoe": {
+        "username": "janedoe",
+        "full_name": "Jane Doe",
+        "email": "janedoe@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    }
+}
