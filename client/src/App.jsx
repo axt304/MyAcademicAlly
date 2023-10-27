2694502bad0e9ad52fe596f4d25d9e0771aaba2e
Parth Dalwadi <dalwadip77@gmail.com>
2023-10-26 20:35:33 -0400

    Created sidebar for dashboard.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './components/home/Home'
 import Missing from './components/missing/Missing'
+import Dashboard from './components/dashboard/Dashboard'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
           <Route exact path="*" element={<Missing />}></Route>
         </Routes>
       </Router>
---
+++ client/src/components/dashboard/Panel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles/Panel.module.css'
+
+const Panel = () => {
+  return (
+    <div id={styles.panel}>
+      <p>Panel</p>
+    </div>
+  )
+}
+
+export default Panel
