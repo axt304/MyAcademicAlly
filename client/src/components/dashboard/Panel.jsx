3070f12d2a65e22a761c2215343c73dad3ae8179
Parth Dalwadi <dalwadip77@gmail.com>
2023-11-05 07:13:06 -0500

    Created EditForm.

--- client/src/components/dashboard/Panel.jsx
+++ client/src/components/dashboard/Panel.jsx
@@ -7,12 +7,13 @@ import { useStoreState, useStoreActions } from 'easy-peasy'
 const Panel = () => {
   const isAddFormOpen = useStoreState((state) => state.isAddFormOpen)
   const setIsAddFormOpen = useStoreActions((actions) => actions.setIsAddFormOpen)
+  const isEditFormOpen = useStoreState((state) => state.isEditFormOpen)
 
   return (
     <div id={styles.panel}>
       <header className={styles.header}>
         <span>Dashboard</span>
-        <FaPlus className={styles.dashboardAdd} onClick={() => setIsAddFormOpen(!isAddFormOpen)}/>
+        <FaPlus className={styles.dashboardAdd} onClick={() => {if (!isEditFormOpen) setIsAddFormOpen(!isAddFormOpen)}}/>
       </header>
 
       <main className={styles.main}>
