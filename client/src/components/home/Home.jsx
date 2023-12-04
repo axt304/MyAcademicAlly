711ad8496f4debbe3a28b45a0e75e3633a0af45e
nhunewgnu <nhunewgnu@gmail.com>
2023-12-03 22:36:10 -0500

    added styling to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css'; // Ensure this is correctly pointing to your CSS file
 
 const Home = () => {
-  return (
-    <>
-      <main>
-        <h2>Home Page</h2>
-      </main>
-    </>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <div className="home-container">
+      <h1 className="main-title">My Academic Ally</h1>
+      <div className="button-container">
+        <div>
+          <button className="btn" onClick={() => handleNavigate('/signup')}>
+            <i className="animation"></i>
+            Sign Up
+            <i className="animation"></i>
+          </button>
+        </div>
+        <div>
+          <button className="btn" onClick={() => handleNavigate('/login')}>
+            <i className="animation"></i>
+            Login
+            <i className="animation"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
